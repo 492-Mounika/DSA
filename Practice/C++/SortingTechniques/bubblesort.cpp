f97e8a78a2e3968516d90b26c9af9340d6c7e416
Mounika Imadabathuni <147241836+492-Mounika@users.noreply.github.com>
2026-08-02 21:22:40 +0530

    Update bubblesort.cpp

--- Practice/C++/SortingTechniques/bubblesort.cpp
+++ Practice/C++/SortingTechniques/bubblesort.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void bubblesort(vector<int> &nums, int n){
+  for(int i = 0;i<n;i++){
+    for(int j = 0;j<n-i-1;j++){
+      if(nums[j] > nums[j+1]) swap(nums[j],nums[j+1]);
+    }
+  }
+}
+int main(){
+  vector<int> nums = {6,4,3,9,2};
+  int n = nums.size();
+  bubblesort(nums,n);
+  for(int x : nums){
+    cout << x << " ";
+  }
+  return 0;
+}
