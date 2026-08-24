93878cb26949de7376434762f07ec48ab50d1368
Mounika Imadabathuni <147241836+492-Mounika@users.noreply.github.com>
2026-08-24 11:52:05 +0530

    Implement checkInclusion for string permutations
    Added function to check if all permutations of s1 are in s2 using a sliding window approach.

--- Practice/C++/Strings/allspermutations.cpp
+++ Practice/C++/Strings/allspermutations.cpp
@@ -19,3 +19,20 @@ void generate_permutations(int idx,string &s,vector<string> &res){
         sort(res.begin(),res.end());
         return res;  
     }
+
+
+//all permutations of s1 in s2 using sliding window
+bool checkInclusion(string s1, string s2) {
+        vector<int> cs1(26),cs2(26);
+        if(s1.size()>s2.size()) return false;
+        for(int i = 0;i<s1.size();i++){
+                cs1[s1[i]-'a']++;
+                cs2[s2[i]-'a']++;
+        }
+        for(int i = s1.size();i<s2.size();i++){
+                if(cs1 == cs2) return true;
+                cs2[s2[i]-'a']++;
+                cs2[s2[i-s1.size()]-'a']--;
+        }
+        return cs1 == cs2;
+}
